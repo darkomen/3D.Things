4a2858f320e8af59322141ed523c10fdeb89a9c3
Santiago López <santiago.lopez.pina@gmail.com>
2013-08-19 00:09:36 +0200

    Ultima versión del gramil

---
+++ Gramil/gramil.scad
@@ -0,0 +1,81 @@
+difference() {
+  difference() {
+    difference() {
+      difference() {
+        difference() {
+          union() {
+            difference() {
+              union() {
+                translate(v=[0.000, -4.020, 0.000]) {
+                rotate([-180.000,-180.000,-180.000]){
+                  cylinder(h=12.000, r1=8.010, r2=8.010, $fn=6, center=true);
+                } // End rotate
+                } // End translate
+                cube(size=[15.990, 8.000, 12.000], center=true);
+                translate(v=[0.000, 10.000, 0.000]) {
+                rotate([-180.000,-180.000,-180.000]){
+                  cube(size=[16.000, 28.000, 12.000], center=true);
+                } // End rotate
+                } // End translate
+              } // End union
+              translate(v=[0.000, -7.636, 0.000]) {
+              rotate([-90.000,-180.000,-180.000]){
+                union() {
+                  cylinder(h=3.500, r1=3.505, r2=3.505, $fn=6, center=true);
+                  cylinder(h=10.000, r1=1.600, r2=1.600, $fn=90, center=true);
+                } // End union
+              } // End rotate
+              } // End translate
+            } // End difference
+            color([0.150, 0.150, 0.150, 1.000]) {
+              translate(v=[0.000, 20.000, 0.000]) {
+              rotate([-90.000,-180.000,-180.000]){
+                cylinder(h=3.000, r1=3.505, r2=3.505, $fn=6, center=true);
+              } // End rotate
+              } // End translate
+            } // End color
+          } // End union
+          translate(v=[0.000, 20.000, 0.000]) {
+          rotate([-90.000,-180.000,-180.000]){
+            cylinder(h=15.000, r1=1.600, r2=1.600, $fn=90, center=true);
+          } // End rotate
+          } // End translate
+        } // End difference
+        translate(v=[-0.000, 20.000, 4.000]) {
+        rotate([-180.000,-180.000,-180.000]){
+          cube(size=[7.000, 3.000, 13.000], center=true);
+        } // End rotate
+        } // End translate
+      } // End difference
+      translate(v=[0.000, -4.020, 0.000]) {
+      rotate([-180.000,-180.000,-180.000]){
+        cylinder(h=16.000, r1=4.000, r2=4.000, $fn=6, center=true);
+      } // End rotate
+      } // End translate
+      translate(v=[6.000, 0.000, 0.000]) {
+      rotate([-180.000,-180.000,-180.000]){
+        cube(size=[20.000, 8.000, 16.000], center=true);
+      } // End rotate
+      } // End translate
+      translate(v=[0.000, 4.020, 0.000]) {
+      rotate([-180.000,-180.000,-180.000]){
+        cylinder(h=16.000, r1=4.000, r2=4.000, $fn=50, center=true);
+      } // End rotate
+      } // End translate
+    } // End difference
+    color([0.150, 0.150, 0.150, 1.000]) {
+      translate(v=[0.000, 14.000, 0.000]) {
+      rotate([-146.310,90.000,-146.310]){
+        cylinder(h=25.000, r1=4.500, r2=4.500, $fn=50, center=true);
+      } // End rotate
+      } // End translate
+    } // End color
+  } // End difference
+  color([0.150, 0.150, 0.150, 1.000]) {
+    translate(v=[0.000, 4.020, 0.000]) {
+    rotate([-180.000,-180.000,-180.000]){
+      cylinder(h=25.000, r1=4.000, r2=4.000, $fn=50, center=true);
+    } // End rotate
+    } // End translate
+  } // End color
+} // End difference
--- Gramil/main.cpp
+++ Gramil/main.cpp
@@ -3,55 +3,105 @@
 #include <ooml/components.h>
 #include <components/RoundedTablet.h> 
 #include <parts/Nuts.h>
+//#include <PolygonalPiramid.h>
+
+
 
 #include <fstream>
 #include <iostream>
  
 using namespace std;
 //Declaración de variables.
-int altura = 20;
-int radio = 8;
+int altura = 12;
+int radio = 4;
 int thickness = 4;
 
 
 
 int main()
 {
-	IndentWriter body_writer;
+	IndentWriter sujeccion_writer;
+	IndentWriter soporte_writer;
 	
+	///////////////////////////////////////
+	///////////////////////////////////////    				
+    ///////////////////////////////////////
+    // Programacion de la sujeccion
+
 	// Cuerpo principal del gramil
-	Component prisma = Cylinder(radio+thickness, altura, 6, true).translate(0,-((radio+thickness))/1.99,0)   //Hexagono
-					 + Cube(((radio+thickness)*2), (radio+thickness), altura,true)   //Cubo
+	Component prisma = Cylinder((0.01+radio+thickness), altura, 6, true).translate(0,-((radio+thickness))/1.99,0)   //Hexagono
+					 + Cube(((radio+thickness+7.99)), (radio+thickness), altura,true)   //Cubo
 					 //+ Cylinder((radio+thickness), altura, 50, true).translate(0,((radio+thickness))/1.99,0) ; //Cilindro
-					 + Cube(24, (radio+thickness+9) ,altura, true).translate(0,((radio+thickness))/1.99,0) ; //Cilindro
+					 + Cube(16, (radio+thickness+20) ,altura, true).translate(0,((radio+thickness))/0.8,0) ; //Cilindro
+
 	Component agujero = Cylinder(radio, altura+thickness, 6, true).translate(0,-(radio+thickness)/1.99,0)  //Hexagono
 					  + Cube(((radio+thickness+2)*2),(radio+thickness),altura+thickness,true).translate(thickness+2,0,0)  //cubo
 					  + Cylinder(radio, altura+thickness, 50, true).translate(0,((radio+thickness))/1.99,0) ;   // Cilindro
 	//Links
 	prisma.addLink(RefSys(0,-((((radio+thickness)/2.2)+((radio+thickness)/2)+((radio-thickness)/1.99))),0)); //Link 0
+	prisma.addLink(RefSys(0,14,0)); //Link 1
+	prisma.addLink(RefSys(0,20,0)); //Link 2
+	Component tuerca = NutShape(M3,3.5)
+					 + Cylinder(1.6,10,90,true);
+
+
 	
-	Component tuerca = NutShape(M3,3).moveToLink(prisma,0).relRotate(90,0,0)
-					 + Cylinder(2.8,10,90,true).moveToLink(prisma,0).relRotate(90,0,0) ;
-
-
-	//body_writer << LinksView(prisma);
-    body_writer <<  prisma 
-    				- tuerca
+    Component Sujecion =	    prisma 
+    				- tuerca.moveToLink(prisma,0).relRotate(90,0,0)
+    				+ NutShape(M3,3).moveToLink(prisma,2).relRotate(90,0,0).color(0.150,0.150,0.150,true) //tuerca emptrada p ara el lapicero
+    				- Cylinder(1.6,15,90,true).moveToLink(prisma,2).relRotate(90,0,0)  //tuerca emptrada p ara el lapicero
+    				- Cube(7,3,13,true).moveToLink(prisma,2).relTranslate(0,0,4) //tuerca emptrada p ara el lapicero
     				- agujero
-    				//+ Cylinder(radio, 100, 50, true).translate(0,((radio+thickness))/1.99,0).color(0.150,0.150,0.150,true) 
-    				//+ Cylinder(2.8,30,90,true).moveToLink(prisma,0).relRotate(90,0,0).color(0.150,0.150,0.150,true)
-
+    				- Cylinder(4.5, 25, 50, true).moveToLink(prisma,1).relRotate(0,90,0).color(0.150,0.150,0.150,true) //Lapicero
+    				- Cylinder(radio, 25, 50, true).translate(0,((radio+thickness))/1.99,0).color(0.150,0.150,0.150,true)  //Mastil
     				;
  
+    ///////////////////////////////////////
+	///////////////////////////////////////    				
+    ///////////////////////////////////////
+    // Programacion del soporte.
+
+    Component cruz = Cube(100,14,5,true)
+    				+ Cube(100,14,5,true).relRotate(0,0,90);
+    Component triangulo = RegularPrism(3, 30.0, 14.0).rotate(0,-90,0).translate(7,0,13.0);
+    
+
+ 	
+	Component soporte = cruz
+					+ triangulo
+					+ triangulo.rotatedCopy(0,0,90)
+					- Cylinder(3.9,61,90,false).translate(0,0,2.5).color(0.150,0.150,0.150,true)
+					- Cube(20,20,36).translate(0,0,40)
+					
+
+	;
+
+ 	///////////////////////////////////////
+	///////////////////////////////////////    				
+    ///////////////////////////////////////
+
+ 	//body_writer << LinksView(prisma);
+ 	sujeccion_writer << Sujecion;
+ 	//body_writer << soporte;
 	ofstream file("gramil.scad");
 	if (file){
 		//generate OpenScad code of the translated cube and save it into file;
-		file << body_writer;
+		file << sujeccion_writer;
 		file.close();
 	}else{
 		cerr << "Error, cannot open the file" << endl;
 	}
  
+ 	soporte_writer << soporte;
+ 	
+	ofstream file2("soporte.scad");
+	if (file2){
+		//generate OpenScad code of the translated cube and save it into file;
+		file2 << soporte_writer;
+		file2.close();
+	}else{
+		cerr << "Error, cannot open the file" << endl;
+	}
 	cout << "Done" << endl;
  
 	return 0;
